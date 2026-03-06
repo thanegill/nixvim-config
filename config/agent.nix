6cf6f908ded7ff5ecf4a3f670b20ee49fef1ffcc
Thane Gill <me@thanegill.com>
2026-03-06 10:50:25 -0800

    agent: add plugins.avante

--- config/agent.nix
+++ config/agent.nix
@@ -1,6 +1,29 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: lib.mkMerge [
 
-  # https://nix-community.github.io/nixvim/plugins/codecompanion/settings.html
+{
+  # https://nix-community.github.io/nixvim/plugins/avante/index.html
+  # https://github.com/yetone/avante.nvim
+  plugins.avante = {
+    enable = true;
+    settings = {
+      provider = "lmstudio";
+      auto_suggestions_provider = "lmstudio";
+      providers = {
+        # https://github.com/yetone/avante.nvim/issues/2488
+        lmstudio = {
+          __inherited_from = "openai";
+          endpoint = "http://localhost:1234/v1";
+          api_key_name = "";
+            model = "qwen3.5-35b-a3b@q8_0";
+        };
+      };
+    };
+  };
+}
+
+{
+  # https://nix-community.github.io/nixvim/plugins/codecompanion/index.html
+  # https://github.com/olimorris/codecompanion.nvim/
   plugins.codecompanion = {
     enable = true;
     settings = {
