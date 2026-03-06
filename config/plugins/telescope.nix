f1cd56689324ad434261c22047282f4a4ec703c3
Thane Gill <me@thanegill.com>
2026-03-06 10:37:20 -0800

    telescope: move <leader>/ keymap to plugin.telescope.keymap

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -40,16 +40,17 @@
     # See `:help telescope.builtin`
     keymaps = {
       "<leader><leader>" = { mode = "n"; action = "find_files"; options = { desc = "[S]earch [F]iles"; }; };
-      "<leader>sb" = { mode = "n"; action = "buffers"; options = { desc = "[S] [B]uffers"; }; };
-      "<leader>sh" = { mode = "n"; action = "help_tags"; options = { desc = "[S]earch [H]elp"; }; };
-      "<leader>sk" = { mode = "n"; action = "keymaps"; options = { desc = "[S]earch [K]eymaps"; }; };
       "<leader>sf" = { mode = "n"; action = "find_files"; options = { desc = "[S]earch [F]iles"; }; };
       "<leader>ss" = { mode = "n"; action = "builtin"; options = { desc = "[S]earch [S]elect Telescope"; }; };
+      "<leader>sb" = { mode = "n"; action = "buffers"; options = { desc = "[S]earch [B]uffers"; }; };
+      "<leader>sh" = { mode = "n"; action = "help_tags"; options = { desc = "[S]earch [H]elp"; }; };
+      "<leader>sk" = { mode = "n"; action = "keymaps"; options = { desc = "[S]earch [K]eymaps"; }; };
       "<leader>sw" = { mode = "n"; action = "grep_string"; options = { desc = "[S]earch current [W]ord"; }; };
       "<leader>sg" = { mode = "n"; action = "live_grep"; options = { desc = "[S]earch by [G]rep"; }; };
       "<leader>sd" = { mode = "n"; action = "diagnostics"; options = { desc = "[S]earch [D]iagnostics"; }; };
       "<leader>sr" = { mode = "n"; action = "resume"; options = { desc = "[S]earch [R]esume"; }; };
       "<leader>s." = { mode = "n"; action = "oldfiles"; options = { desc = "[S]earch Recent Files ('.' for repeat)"; }; };
+      "<leader>/" = { mode = "n"; action = "current_buffer_fuzzy_find"; options = { desc = "[/] fuzzily search in current buffer"; }; };
     };
 
     settings.extensions.__raw = "{ ['ui-select'] = { require('telescope.themes').get_dropdown() } }";
@@ -57,25 +58,6 @@
   };
 
   keymaps = [
-    # Slightly advanced example of overriding default behavior and theme
-    # You can pass additional configuration to Telescope to change the theme, layout, etc.
-    {
-      mode = "n";
-      key = "<leader>/";
-      action.__raw = ''
-        function()
-          require('telescope.builtin').current_buffer_fuzzy_find(
-            require('telescope.themes').get_dropdown {
-              winblend = 10,
-              previewer = false
-            }
-          )
-        end
-      '';
-      options = {
-        desc = "[/] Fuzzily search in current buffer";
-      };
-    }
     # It's also possible to pass additional configuration options.
     # See `:help telescope.builtin.live_grep()` for information about particular keys
     {
