9e998b131bb4cfee997f94c20f06f4cad303215b
Thane Gill <me@thanegill.com>
2026-03-29 13:16:20 -0700

    Move editing.search/replace -> search

--- config/editing.nix
+++ config/editing.nix
@@ -122,42 +122,6 @@
     '';
   }
 
-  { # Search/replace
-
-    # Preview substitutions live, as you type!
-    opts.inccommand = "split";
-
-    keymaps = [
-      {
-        mode = "n";
-        key = "\#";
-        action = ''*:%s/<C-r>///g<Left><Left>'';
-        options.desc = "Search and replace new word under cursor";
-      }
-      # Like "#", but don't put "\<" and "\>" around the word.
-      {
-        mode = "n";
-        key = "g\#";
-        action = ''g*:%s/<C-r>///g<Left><Left>'';
-        options.desc = "Search and replace new text under cursor";
-      }
-      # Like "#", but put the word in the replacement side
-      {
-        mode = "n";
-        key = "gs";
-        action = ''*:%s/<C-r>//<C-r>//g<Left><Left>'';
-        options.desc = "Search and replace old word under cursor";
-      }
-      # And for visual: https://stackoverflow.com/questions/676600/vim-search-and-replace-selected-text
-      {
-        mode = "v";
-        key = "\#";
-        action = ''""y/<C-r>=escape(@", '/\')<CR>:%s/<C-r>///g<Left><Left>'';
-        options.desc = "Search and replace old word under cursor";
-      }
-    ];
-  }
-
   { # Clipboard
 
     keymaps = [ { key = "<leader>pp"; action = ":setlocal paste!"; } ];
--- config/search.nix
+++ config/search.nix
@@ -7,14 +7,40 @@
     ignorecase = true;
     smartcase = true;
 
+    # Preview substitutions live, as you type!
+    inccommand = "split";
+
   };
   keymaps = [
-    # Clear highlights on search when pressing <Esc> in normal mode
-    #  See `:help hlsearch`
     {
       mode = "n";
       key = "<Esc>";
       action = "<cmd>nohlsearch<CR>";
+      options.desc = "Clear search highlights";
+    }
+    {
+      mode = "n";
+      key = "\#";
+      action = ''*:%s/<C-r>///g<Left><Left>'';
+      options.desc = "Search and replace new word under cursor";
+    }
+    { # Like "#", but don't put "\<" and "\>" around the word.
+      mode = "n";
+      key = "g\#";
+      action = ''g*:%s/<C-r>///g<Left><Left>'';
+      options.desc = "Search and replace new text under cursor";
+    }
+    { # Like "#", but put the word in the replacement side
+      mode = "n";
+      key = "gs";
+      action = ''*:%s/<C-r>//<C-r>//g<Left><Left>'';
+      options.desc = "Search and replace old word under cursor";
+    }
+    { # And for visual: https://stackoverflow.com/questions/676600/vim-search-and-replace-selected-text
+      mode = "v";
+      key = "\#";
+      action = ''""y/<C-r>=escape(@", '/\')<CR>:%s/<C-r>///g<Left><Left>'';
+      options.desc = "Search and replace old word under cursor";
     }
   ];
 }
